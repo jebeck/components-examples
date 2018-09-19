1be6089dffcbd5fef602830a15688e70bd85ae41
Jana Beck <jana.beck@stitchfix.com>
2018-09-18 17:21:34 -0700

    add ability to POST

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { json, send, sendError } = require("micro")
-const cors = require("micro-cors")({ allowHeaders: ["query"] })
+const cors = require("micro-cors")({
+  allowHeaders: ["Content-Type", "query"],
+  allowMethods: ["DELETE", "GET", "POST"],
+})
 const MongoClient = require("mongodb").MongoClient
 const qs = require("qs")
 
@@ -25,17 +28,30 @@ async function getMongoClient() {
 }
 
 const handler = async (req, res) => {
+  const parsed = await json(req)
   const { table } = qs.parse(req.url.split("?")[1])
 
   const mongoDb = await getMongoClient()
 
-  mongoDb
-    .collection(table)
-    .find({})
-    .limit(JSON.parse(req.headers.query).limit || 0)
-    .toArray()
-    .then(docs => send(res, 200, docs))
-    .catch(sendError)
+  if (req.method === "POST") {
+    mongoDb
+      .collection(table)
+      .insertOne(parsed, { forceServerObjectId: true })
+      .then(resp => send(res, 200, resp))
+      .catch(err => {
+        throw err
+      })
+  } else {
+    mongoDb
+      .collection(table)
+      .find({})
+      .limit(JSON.parse(req.headers.query).limit || 0)
+      .toArray()
+      .then(docs => send(res, 200, docs))
+      .catch(err => {
+        throw err
+      })
+  }
 }
 
 module.exports = cors(handler)
